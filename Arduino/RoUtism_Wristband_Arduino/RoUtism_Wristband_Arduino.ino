7d0753cd4221690f947f4fbab59d67b7f7dd57b3
Deniz K Acikbas <denizkarya1999@outlook.com>
2025-02-24 20:51:20 -0500

    <DKA and Dr. Zhang - 02.22.2025> Achieved the goal for Arduino code

--- Arduino/RoUtism_Wristband_Arduino/RoUtism_Wristband_Arduino.ino
+++ Arduino/RoUtism_Wristband_Arduino/RoUtism_Wristband_Arduino.ino
@@ -1,78 +1,22 @@
-#include "esp_sleep.h"  // Required for deep sleep functions 
-
-const int redLED = D2;    // Red LED pin
-const int greenLED = D4;  // Green LED pin
-
-float shutterRate = 5000.0; // Set frequency to 5 kHz
-float shutterPeriod;        // Shutter period in microseconds
-int pulseWidth;             // Width of each ON pulse (50% duty cycle)
-int gapWidth;               // Width of each OFF gap (50% duty cycle)
-
-unsigned long loopStartTime; // To mark the beginning of the 45-second cycle
-
-// Calculate timing parameters based on shutterRate (assumes 50% duty cycle)
-void calculateTiming() {
-  shutterPeriod = (1.0 / shutterRate) * 1e6; // period in microseconds
-  pulseWidth = shutterPeriod / 2;            // 50% ON time
-  gapWidth = shutterPeriod / 2;              // 50% OFF time
-}
-
-// Emit an OOK symbol using the specified binary pattern on a given LED pin
-// '1' turns the LED ON for pulseWidth and '0' turns it OFF for gapWidth.
-void emitSymbol(int ledPin, const char* binaryPattern) {
-  for (int i = 0; binaryPattern[i] != '\0'; i++) {
-    if (binaryPattern[i] == '1') {
-      digitalWrite(ledPin, HIGH);
-      delayMicroseconds(pulseWidth);
-    } else {
-      digitalWrite(ledPin, LOW);
-      delayMicroseconds(gapWidth);
-    }
-  }
-}
-
-// Generate one OOK sequence on the given LED (here, greenLED)
-void generateOOKSignal(int ledPin) {
-  // Using a sample binary pattern "10101010" as an example.
-  emitSymbol(ledPin, "10101010");
-}
-
-// Create a red preamble by toggling the red LED at 5 kHz for 1 second.
-void redPreamble() {
-  unsigned long start = micros();
-  while ((micros() - start) < 1000000UL) { // run for 1 second
-    digitalWrite(redLED, HIGH);
-    delayMicroseconds(pulseWidth);
-    digitalWrite(redLED, LOW);
-    delayMicroseconds(gapWidth);
-  }
-}
+// define led according to pin diagram in article
+const int redLed2 = D2; // there is no LED_BUILTIN available for the XIAO ESP32C3.
+const int greenLed4 = D4;
 
 void setup() {
-  pinMode(redLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  calculateTiming();
-  loopStartTime = millis();  // Record the starting time of the 45-second cycle
+  // initialize digital pin led as an output
+  pinMode(redLed2, OUTPUT);
+  pinMode(greenLed4, OUTPUT);
 }
 
 void loop() {
-  // Continue the process until 45 seconds have passed
-  while (millis() - loopStartTime < 45000UL) {
-    // 1. Emit the red preamble.
-    redPreamble();
-
-    // 2. Emit 5 OOK sequences on the green LED.
-    for (int i = 0; i < 5; i++) {
-      generateOOKSignal(greenLED);
-      // Optionally, add a short delay between sequences.
-      delay(100);
-    }
-  }
-
-  // Turn off both LEDs after 45 seconds.
-  digitalWrite(redLED, LOW);
-  digitalWrite(greenLED, LOW);
-  
-  // If you want to use deep sleep, you can uncomment the next line.
-  esp_deep_sleep_start();
+    digitalWrite(redLed2, LOW);   // 0 turn off the Red Led 
+    delayMicroseconds(166);
+    digitalWrite(greenLed4, HIGH);   // 1  turns on the Green LED
+    delayMicroseconds(166);                
+    digitalWrite(greenLed4, LOW);    // 0  turns off the Green LED
+    delayMicroseconds(166);
+    digitalWrite(greenLed4, HIGH);   // 1  turns on the Green LED
+    delayMicroseconds(166);                  
+    digitalWrite(greenLed4, LOW);  // 0  turns off the Green LED
+    delayMicroseconds(166);
 }
