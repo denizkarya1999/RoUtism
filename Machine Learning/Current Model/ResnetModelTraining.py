0366944f95a8b39acfe8b8997fe452178ace7077
Deniz K Acikbas <denizkarya1999@outlook.com>
2025-06-25 14:11:07 -0400

    <DKA - June 25th 2025> More experiments and data added.

--- Machine Learning/Current Model/ResnetModelTraining.py
+++ Machine Learning/Current Model/ResnetModelTraining.py
@@ -49,7 +49,7 @@ def main():
     }
 
     # 2. Datasets & Loaders
-    data_dir = 'three_class_dataset'
+    data_dir = 'facial_data'
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), transform=data_transforms[x]) for x in ['train','val']}
     dataloaders = {
         'train': DataLoader(image_datasets['train'], batch_size=32, shuffle=True,
